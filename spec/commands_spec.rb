afc1f6b85078bc0cd003cb16fd6b815758fe411b
Toby Clemson <tobyclemson@gmail.com>
2020-08-28 14:56:21 +0100

    Add a basic test.

---
+++ spec/commands_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'commands' do
+  image = 'node-exporter-aws:latest'
+  extra = {
+      'Entrypoint' => '/bin/sh',
+  }
+
+  before(:all) do
+    set :backend, :docker
+    set :docker_image, image
+    set :docker_container_create_options, extra
+  end
+
+  after(:all, &:reset_docker_backend)
+
+  it "includes the node_exporter command" do
+    expect(command('/opt/node-exporter/bin/node_exporter --version').stderr)
+        .to(match(/1.0.1/))
+  end
+
+  def reset_docker_backend
+    Specinfra::Backend::Docker.instance.send :cleanup_container
+    Specinfra::Backend::Docker.clear
+  end
+end
